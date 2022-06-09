ada4994633709da3bece531277b9584249b77440
bo.liu <373431485>
2022-06-09 17:55:17 +0800

    初始化工程

---
+++ src/main/java/com/kafeim/auth/KafeimAuthApplication.java
@@ -0,0 +1,13 @@
+package com.kafeim.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafeimAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafeimAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kafeim/auth/KafeimAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafeim.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafeimAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
